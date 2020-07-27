3423ddd537631a429e9a37f915d8789c706ca100
俊広 塩月 <shiotsuki@worldmarksolutions.com>
2020-07-27 15:42:05 +0900

    new

--- src/main/java/com/main/LineBotController.java
+++ src/main/java/com/main/LineBotController.java
@@ -1,17 +1,28 @@
 package com.main;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.factory.DBFactory;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
+import com.utility.Constants;
+import com.utility.DBConnection;
 
 /**
  * @author shiotsuki
@@ -37,13 +48,68 @@ public class LineBotController {
 		String value = "<" + request.getParameter("server").toString() + ">\r\n";
 		value += request.getParameter("text").toString();
 
-		try {
-			response = this.lineMessagingClient
-			        .pushMessage(new PushMessage("Ud6e699869888126beedab30c5b3d484e".toString(),
-			                     new TextMessage(value.toString()
-			                      ))).get();
+    	DBConnection conn = null;
+    	PreparedStatement ps = null;
+    	ResultSet rs = null;
+    	String text = "";
+    	StringBuilder sbFindSQL = null;
+    	Resource resource = null;
+    	Properties props = null;
 
+		try
+		{
+        	resource = new ClassPathResource(Constants.PROP_PATH);
+        	props = PropertiesLoaderUtils.loadProperties(resource);
+			conn = DBFactory.getConnection(props);
+
+
+			if (value.isEmpty() == false)
+			{
+				String bot_id = props.getProperty("id").toString();
+
+				// ユーザ情報を取得
+				String tb_user = conn.GetProps().getProperty("tb.user");
+		    	sbFindSQL = new StringBuilder();
+				sbFindSQL.delete(0, sbFindSQL.length());
+				sbFindSQL.append("SELECT user_id, authority FROM ");
+				sbFindSQL.append(tb_user.toString());
+				sbFindSQL.append(" WHERE permissions =? AND bot_id=?");
+
+				ps = conn.getPreparedStatement(sbFindSQL.toString(), null);
+				if (ps != null)
+				{
+					ps.clearParameters();
+					ps.setInt(1, 1);
+					ps.setString(2, bot_id.toString());
+					rs = ps.executeQuery();
+					if (rs != null)
+					{
+						// ラインへプッシュ
+						while (rs.next())
+						{
+							String user_id = rs.getString("user_id").toString();
+
+							response = this.lineMessagingClient
+							        .pushMessage(new PushMessage(user_id.toString(),
+							                     new TextMessage(value.toString()
+							                      ))).get();
+						}
+					}
+				}
+			}
 		} catch (InterruptedException | ExecutionException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
